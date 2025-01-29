8b5dd2632b0310b226e00e938b58b4919acb1d93
FelipeWelter <feliwelter45@gmail.com>
2025-01-29 18:17:15 -0300

    Soma de dois numeros, de duas formas diferentes

---
+++ Day2/soma_dois_numeros2.py
@@ -0,0 +1,9 @@
+def somar_dois_num():
+    numero1 = int(input("coloque o primeiro numero: "))
+    numero2 = int(input("coloque o segundo numero: "))
+    somar_dois_numeros = numero1 + numero2
+    print(somar_dois_numeros)
+    return somar_dois_numeros
+    
+somar_dois_num()
+
