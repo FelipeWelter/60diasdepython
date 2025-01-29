99a8a37ab50e0a2f01707c7652ef50645bde2bff
FelipeWelter <feliwelter45@gmail.com>
2025-01-29 18:47:23 -0300

    adicionando dia 4 laco de repeticao

---
+++ Day4/laco_de_repeticao.py
@@ -0,0 +1,11 @@
+#usando for para impressao
+
+for numero in range(11):
+    print(numero)
+    
+#usandoo o while para imprimir numeros de 0 a 10
+
+numero = 0
+while numero < 11:
+    print(numero)
+    numero += 1
