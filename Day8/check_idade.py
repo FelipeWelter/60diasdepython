056f5b302119b644bf3ad15d0c508c8b683b795e
FelipeWelter <feliwelter45@gmail.com>
2025-02-05 18:00:13 -0300

    Funcao para verificar maior ou menor

---
+++ Day8/check_idade.py
@@ -0,0 +1,8 @@
+#solicitar a idae e verificar se pode dirigir
+
+idade = int(input("Digite a sua idade: "))
+
+if idade >= 18:
+    print("voce pode dirigir")
+else:
+    print("voce nao pode dirigir")
